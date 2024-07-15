c7f28b66cab2e685bdc7f02b556f8d16046e6971
tamer ugur <tamerugur3@hotmail.com>
2024-07-15 19:59:49 +0300

    created some base classes, added fake data to see if the code works.

---
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ECommerceAPI.Application.Abstractions;
+using ECommerceAPI.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection Services)
+        {
+            Services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
